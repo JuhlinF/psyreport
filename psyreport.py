2ff0c78b46a9fc35130ea3eaf5d7a68f8bc71476
JuhlinF <60777254+JuhlinF@users.noreply.github.com>
2023-05-10 08:10:33 +0000

    Initial report code.

---
+++ psyreport.py
@@ -0,0 +1,25 @@
+"""
+Create a report based on a Pearson Q-Interactive export file using Jinja2
+"""
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+from psyscore import parse_pearson_zipfile
+
+
+def create_report(qi_file: str, template_name: str):
+    """
+    Create a report for test in <qi_file> using Jinja2 template <template_name>
+    """
+    battery = parse_pearson_zipfile(qi_file)
+    env = Environment(
+        loader=PackageLoader("psyreport"),
+        trim_blocks=True,
+        autoescape=select_autoescape(),
+    )
+    template = env.get_template(template_name)
+
+    return template.render(battery=battery)
+
+
+if __name__ == "__main__":
+    print(create_report("tests/EW202304_4_19_2023.zip", "plain.txt"))
